480db8a7d304c3b3a06f232b63305e75d6e32ffc
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2022-04-24 17:32:02 +0800

    Create peak_calling.R

---
+++ R/peak_calling.R
@@ -0,0 +1,6 @@
+result =exomePeak2(bam_ip = IP_BAM,
+           bam_input = INPUT_BAM,
+           gff_dir = GENE_ANNO_GTF,
+           genome = "mm10",
+           consistent_peak = TRUE,
+           paired_end = FALSE)
