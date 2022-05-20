90a4ff5fec5bc9c69328298d4a3d0960206cf430
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2022-05-20 22:17:19 +0800

    Update Quantify_expression.R

--- R/Quantify_expression.R
+++ R/Quantify_expression.R
@@ -1,7 +1,15 @@
-gene_count <- featureCounts(Input_data,isGTFAnnotationFile=TRUE,GTF.featureType="exon",GTF.attrType="gene_name", annot.ext = gtf, isPairedEnd=T, nthreads=20)
+obtain_gene_expression <- function(Input_data, GTF_file, nthreads=20, isPairedEnd=F){
+  gene_count <- featureCounts(Input_data=Input_data,isGTFAnnotationFile=TRUE,GTF.featureType="exon",
+                              GTF.attrType="gene_name", annot.ext = GTF_file, 
+                              isPairedEnd=isPairedEnd, nthreads=nthreads)
 counts_data <- gene_count$counts
-colnames(counts_data) <- c("Im_rep1","Ma_rep1")
 countMatrix <- sapply(as.matrix(counts_data), as.numeric)
 countmatrixgene <- matrix(countMatrix, nrow=nrow(counts_data), ncol = ncol(counts_data))
 colnames(countmatrixgene) <- colnames(counts_data)
 rownames(countmatrixgene) <- rownames(counts_data)
+return(countmatrixgene)  
+}
+
+  
+
+
