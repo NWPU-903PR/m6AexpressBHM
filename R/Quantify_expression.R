7ee203625c0b439db36f7729471f7efc0649c7bf
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2022-05-21 20:42:35 +0800

    Update Quantify_expression.R

--- R/Quantify_expression.R
+++ R/Quantify_expression.R
@@ -1,5 +1,5 @@
 obtain_gene_expression <- function(Input_data, GTF_file, nthreads=20, isPairedEnd=F){
-  gene_count <- featureCounts(Input_data=Input_data,isGTFAnnotationFile=TRUE,GTF.featureType="exon",
+  gene_count <- featureCounts(Input_data,isGTFAnnotationFile=TRUE,GTF.featureType="exon",
                               GTF.attrType="gene_name", annot.ext = GTF_file, 
                               isPairedEnd=isPairedEnd, nthreads=nthreads)
 counts_data <- gene_count$counts
